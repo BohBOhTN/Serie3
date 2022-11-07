41091cecb7bce5d27b590a43ed9a36564dd799ed
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2022-11-07 19:18:34 +0100

    Update Exercice1.c

--- Exercice1.c
+++ Exercice1.c
@@ -12,34 +12,33 @@ int main()
     scanf("%d",&c);
     if(a>b && a>c){
         if(b>c){
-            printf("Le plus grand nombre est %d \n",a);
-            printf("Le plus petit nombre est %d \n",c);
+            printf("Le plus grand nombre est %d \n",(a*100) + (b*10) + c);
+            printf("Le plus petit nombre est %d \n",(c*100) + (b*10) + a);
         }else{
-            printf("Le plus grand nombre est %d \n",a);
-            printf("Le plus petit nombre est %d \n",b);
+            printf("Le plus grand nombre est %d \n",(a*100) + (c*10) + b);
+            printf("Le plus petit nombre est %d \n",(b*100) + (c*10) + a);
         }
     }else{
         if(b>a && b>c){
             if(a>c){
-                printf("Le plus grand nombre est %d \n",b);
-                printf("Le plus petit nombre est %d \n",c);
+                printf("Le plus grand nombre est %d \n",(b*100) + (a*10) + c);
+                printf("Le plus petit nombre est %d \n",(c*100) + (a*10) + b);
             }else{
-                printf("Le plus grand nombre est %d \n",b);
-                printf("Le plus petit nombre est %d \n",a);
+                printf("Le plus grand nombre est %d \n",(b*100) + (c*10) + a);
+                printf("Le plus petit nombre est %d \n",(a*100) + (c*10) + b);
             }
         }else {
             if(c>a && c>b){
                 if(a>b){
-                    printf("Le plus grand nombre est %d \n",c);
-                    printf("Le plus petit nombre est %d \n",b);
+                    printf("Le plus grand nombre est %d \n",(c*100) + (a*10) + b);
+                    printf("Le plus petit nombre est %d \n",(b*100) + (a*10) + c);
                 }else{
-                    printf("Le plus grand nombre est %d \n",c);
-                    printf("Le plus petit nombre est %d \n",a);
+                    printf("Le plus grand nombre est %d \n",(c*100) + (b*10) + a);
+                    printf("Le plus petit nombre est %d \n",(a*100) + (b*10) + c);
                 }
             }
 
         }
-
     }
     return 0;
 }
